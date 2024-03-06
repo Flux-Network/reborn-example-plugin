be9eeb290ecdbb1cb871efbac4b187b0a420365d
Flux-Network <153965143+Flux-Network@users.noreply.github.com>
2024-03-05 19:54:55 -0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,8 @@
 // @ts-ignore
-PluginAPI.displayToChat({msg: "Hello test"})
+function spiderListener() {
+    if (PluginAPI.player.isCollidedHorizontally) {
+        PluginAPI.player.motionY += 0.2;
+        PluginAPI.player.reload();
+    }
+}
+PluginAPI.addEventListener("update", spiderListener);
